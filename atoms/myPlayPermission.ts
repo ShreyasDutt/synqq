0418b24f85911065f0b372b0b694204726e55d88
Paramvir Singh Thind <paramvirsingh2540@gmail.com>
2026-01-29 16:15:46 -0800

    default tracks issue solve

--- atoms/myPlayPermission.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from "jotai";
-
-export const myPlayPermissionAtom = atom<boolean>(false);
--- components/room/Tabs/MusicTab.tsx
+++ components/room/Tabs/MusicTab.tsx
@@ -1,15 +1,18 @@
 "use client";
 import { getdefaultSongsAction } from "@/app/actions/defaultSongs.action";
 import { roomCodeAtom } from "@/atoms/atoms";
-import { amIAdminAtom, CurrentPlayingSong, roomDataAtom } from "@/atoms/convexQueriesAtoms";
+import {
+  amIAdminAtom,
+  CurrentPlayingSong,
+  roomDataAtom,
+} from "@/atoms/convexQueriesAtoms";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
 import { useAtom } from "jotai";
 import { Minus, Play, Trash } from "lucide-react";
-
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 
 type Song = {
   name: string;
@@ -25,7 +28,8 @@ const MusicTab = () => {
   }
   const [roomData] = useAtom(roomDataAtom);
   const [amIAdmin] = useAtom(amIAdminAtom);
-  const myPlayPermission = amIAdmin || roomData?.room.playbackPermissions === "everyone";
+  const myPlayPermission =
+    amIAdmin || roomData?.room.playbackPermissions === "everyone";
   const [, setCurrentPlayingSong] = useAtom(CurrentPlayingSong);
   const [currentSongId, setcurrentSongId] = useState<Id<"song"> | null>(null);
   const [currentSongDuration, setcurrentSongDuration] = useState("");
@@ -51,7 +55,15 @@ const MusicTab = () => {
     }
   }, [SongUrl]);
 
-  useEffect(() => {}, []);
+  const [defaultSongsPending, startTransition] = useTransition();
+  useEffect(() => {
+    if (!roomData?.room.defaultTracks || defaultSongs.length !== 0) return;
+
+    startTransition(async () => {
+      const data = await getdefaultSongsAction();
+      setDefaultSongs(data);
+    });
+  }, [roomData?.room.defaultTracks]);
 
   return (
     <>
@@ -63,9 +75,7 @@ const MusicTab = () => {
               <p className="text-neutral-400">No tracks yet</p>
               <Button
                 className="rounded-full text-xs px-4 py-2"
-                onClick={async () => {
-                  const songs = await getdefaultSongsAction();
-                  setDefaultSongs(songs);
+                onClick={() => {
                   openDefaultTracksMutation({
                     roomCode,
                     setDefaultTracks: true,
@@ -83,7 +93,7 @@ const MusicTab = () => {
 
         {roomData?.room.defaultTracks && (
           <div>
-            <div className="text-2xl flex justify-center gap-4 px-4">
+            <div className="text-2xl flex justify-center gap-4 pb-2">
               <div>Default Songs</div>
               {myPlayPermission && (
                 <div>
@@ -102,59 +112,65 @@ const MusicTab = () => {
               )}
             </div>
             <div>
-              {defaultSongs.map((song, index) => (
-                <div
-                  key={index}
-                  className={`flex items-center px-4 py-3 rounded-md group transition-colors select-none cursor-pointer ${
-                    myPlayPermission && "hover:bg-muted-foreground/10"
-                  }`}
-                >
+              {defaultSongsPending ? (
+                <p className="flex justify-center items-center pt-7 text-muted-foreground px-4">
+                  Loading default songs...
+                </p>
+              ) : (
+                defaultSongs.map((song, index) => (
                   <div
-                    className="flex items-center flex-1"
-                    // onClick={() => {
-                    //   if (
-                    //     myPlayPermission
-                    //   ) {
-                    //     setcurrentSongId(song._id);
-                    //     setcurrentSongDuration(formattedDuration);
-                    //   }
-                    // }}
+                    key={index}
+                    className={`flex items-center px-4 py-3 rounded-md group transition-colors select-none cursor-pointer ${
+                      myPlayPermission && "hover:bg-muted-foreground/10"
+                    }`}
                   >
-                    {myPlayPermission ? (
-                      <div className="w-6 flex items-center justify-center relative">
-                        {/* Index number */}
-                        <span className="text-sm text-neutral-500 group-hover:opacity-0 transition-opacity">
+                    <div
+                      className="flex items-center flex-1"
+                      // onClick={() => {
+                      //   if (
+                      //     myPlayPermission
+                      //   ) {
+                      //     setcurrentSongId(song._id);
+                      //     setcurrentSongDuration(formattedDuration);
+                      //   }
+                      // }}
+                    >
+                      {myPlayPermission ? (
+                        <div className="w-6 flex items-center justify-center relative">
+                          {/* Index number */}
+                          <span className="text-sm text-neutral-500 group-hover:opacity-0 transition-opacity">
+                            {index + 1}
+                          </span>
+
+                          {/* Play icon (hidden until hover) */}
+                          <Play
+                            size={16}
+                            className="absolute opacity-0 group-hover:opacity-100 transition-opacity text-white"
+                          />
+                        </div>
+                      ) : (
+                        <p className="w-6 text-sm text-neutral-500">
                           {index + 1}
-                        </span>
+                        </p>
+                      )}
 
-                        {/* Play icon (hidden until hover) */}
-                        <Play
-                          size={16}
-                          className="absolute opacity-0 group-hover:opacity-100 transition-opacity text-white"
-                        />
+                      <div className="flex-1 ml-4">
+                        <p className="text-sm">{song?.name}</p>
                       </div>
-                    ) : (
-                      <p className="w-6 text-sm text-neutral-500">
-                        {index + 1}
-                      </p>
-                    )}
+                    </div>
 
-                    <div className="flex-1 ml-4">
-                      <p className="text-sm">{song?.name}</p>
+                    <div className="flex items-center gap-3 text-sm">
+                      <p className="text-neutral-400">{song?.duration}</p>
                     </div>
                   </div>
-
-                  <div className="flex items-center gap-3 text-sm">
-                    <p className="text-neutral-400">{song?.duration}</p>
-                  </div>
-                </div>
-              ))}
+                ))
+              )}
             </div>
           </div>
         )}
 
         {songsList?.length != 0 && (
-          <div className="text-2xl flex justify-center px-4">My Songs</div>
+          <div className="text-2xl flex justify-center pb-2">My Songs</div>
         )}
         {songsList?.map((song, index) => {
           const minutes = Math.floor(song.duration / 60);
